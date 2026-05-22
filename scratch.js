3544843d77a83ff5c816511797eb91a7a57b08bf
Hashlay <contacthashlay@gmail.com>
2026-05-22 21:20:21 +0530

    icons

--- scratch.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('hashlay-admin.html', 'utf8');
-c = c.replace(/👁/g, '<i class="fa-solid fa-eye"></i>');
-c = c.replace(/✏️/g, '<i class="fa-solid fa-pencil"></i>');
-c = c.replace(/🗑️/g, '<i class="fa-solid fa-trash"></i>');
-fs.writeFileSync('hashlay-admin.html', c);
